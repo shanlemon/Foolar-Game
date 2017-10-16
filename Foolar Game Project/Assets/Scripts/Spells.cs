0c51916464eba1b57ed3bc4d0d771046f37bf5d3
lazoh <lazohbeem02@gmail.com>
2017-10-16 14:24:52 -0500

    Created start of Spell class
    that it

---
+++ Foolar Game Project/Assets/Scripts/Spells.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spells : MonoBehaviour {
+
+	public GameObject player;
+	public float power, range;
+	public int cooldown, charges;
+	protected int currentCD, currentCharge;
+
+	void fireProjectile () {
+
+	}
+
+}
