1c16ce64f41472d35d138db2d678288f0b51e6b1
shanlemon <shankmemon@gmail.com>
2017-12-26 15:37:08 -0600

    Added Main menu and Text Mesh Pro
    BIG COMMIT

---
+++ Foolar Game Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static bool settingsOpen;
+    public GameObject settings;
+
+    void Start() {
+        settingsOpen = false;
+    }
+
+    // Update is called once per frame
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            settingsOpen = !settingsOpen;
+            settings.SetActive(settingsOpen);
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+	}
+
+    public void goBack() {
+        settingsOpen = !settingsOpen;
+        settings.SetActive(settingsOpen);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+}
